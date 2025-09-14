eadc341d9efa1ec561f0bc4e58245f2946b6a9a9
Steve Adansi <stevorica1@gmail.com>
2025-09-14 14:33:41 +0000

    Add django-models project with relationshipp_app

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+from django.views.generic import DetailView
+
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
